9ab18a2ee45b07f6c92941f5996bcfb14e1ae9e0
Taheer Mattur <md.taheermattur@gmail.com>
2021-09-21 23:52:53 +0530

    modified design

--- app.js
+++ app.js
@@ -38,7 +38,16 @@ function addTodo(event) {
     doneButton.classList.add("done-btn");
     todoDiv.appendChild(doneButton);
 
-    //Create TRASH BUTTON
+    //Create edit Button
+    const editButton = document.createElement("button");
+    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="pointer-events-none h-5 w-5" viewBox="0 0 20 20" fill="#2908e4dc" stroke="transparent">
+    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+  </svg>`;
+    editButton.classList.add("edit-btn");
+    todoDiv.appendChild(editButton);
+
+    //Create Trash Button
     const trashButton = document.createElement("button");
     trashButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="pointer-events-none h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
     <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
@@ -145,6 +154,15 @@ function getTodos(todo) {
         </svg>`;
         doneButton.classList.add("done-btn");
         todoDiv.appendChild(doneButton);
+
+        // Create Edit button
+        const editButton = document.createElement("button");
+        editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="pointer-events-none h-5 w-5" viewBox="0 0 20 20" fill="#2908e4dc" stroke="transparent">
+        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+      </svg>`;
+        editButton.classList.add("edit-btn");
+        todoDiv.appendChild(editButton);
 
         //Create TRASH BUTTON
         const trashButton = document.createElement("button");
